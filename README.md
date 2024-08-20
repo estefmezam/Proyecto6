d2e1183fb68f06efd93e790271a787b3f14940d0
EstefaniaM <estefaniamezam@gmail.com>
2024-08-19 22:18:43 -0600

    Finish streamlit app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Proyecto6
-Las tareas incluyen la creación y gestión de entornos virtuales de Python.
+Sprint 6: Herramientas de Desarrollo de Software
+En este proyecto vamos a poner a prueba lo aprendido durante el Sprint.
+Debo confesar que muchas cosas fueron nuevas para mi, sobre todo el uso de linea de comandos, 
+sin embargo, me sorprendio lo que podemos lograr usando solo una pantalla negra y unos cuantos
+comandos. 
+El objetivo principal del proyecto, es la creacion y gestion de un entorno virtual en Python, y 
+desarrollar una aplicacion web, asi como hacer uso de la nube. 
+Siguiendo los pasos se logro lo siguiente: 
--- app.py
+++ app.py
@@ -2,15 +2,28 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+#Leer los fatos
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+#Titulo de la aplicacion
+st.header('Analisis Exploratorio de Anuncios de Ventas de Coches')
+
 hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')      
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-        
+    fig = px.histogram(car_data, x="odometer")  
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+    
+scatt_button = st.button('Construir un gráfico de dispersión')
+
+if scatt_button: #al hacer clic en el boton
+    #Escribe el mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    #Crear un histograma
+    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    #Mostrar un grafico Plotly 
+    st.plotly_chart(fig,use_container_width=True) # crear gráfico de dispersión
